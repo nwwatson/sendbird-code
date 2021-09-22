a16a3671b46a4ea4c66031ede0c6a29f7a88335b
Nicholas W. Watson <nwwatson@hey.com>
2021-09-22 13:49:53 -0400

    updated consts per todd

--- src/const.js
+++ src/const.js
@@ -1,12 +1,12 @@
 // put your own APP_ID here
 // get your app_id -> https://dashboard.sendbird.com/auth/signin
-export const APP_ID = "65840362-69D4-4253-B261-128D197080DB";
+export const APP_ID = "C693AA79-6B78-4F12-95B1-C0CC633E7E0F";
 
 // set your own USER_ID and NICKNAME
 export const USER_ID = "friend1";
 export const NICKNAME = "friend1";
 
-export const YOUR_SALESFORCE_URL = "https://daytwo--dev--c.container.lightning.com";
+export const YOUR_SALESFORCE_URL = "https://daytwo.lightning.force.com";
 
 export default {
   APP_ID,
